ff2bafdca8c4492a5e15117f7fcf75a2c52e761a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-10 13:50:31 -0300

    feat: mejorar el endpoint /video

--- src/routes/videoRouter.ts
+++ src/routes/videoRouter.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express'
 import { videoThumbnailRouter } from './videoThumbnailRouter'
+import { ROUTER_ERRORS, REQUEST_EXAMPLES } from '../lib/constants'
+import { getMessage } from '../lib/displayMessages'
 
 export const videoRouter = Router()
 
-videoRouter.get('/', (req, res) => {
-  res.send('video router /')
+videoRouter.get('/', (_, res) => {
+  res.status(400)
+  res.json({
+    success: false,
+    message: getMessage(ROUTER_ERRORS.MISSING_VIDEO_ID),
+    example: REQUEST_EXAMPLES.VIDEO
+  })
 })
 
 videoRouter.use('/thumbnail', videoThumbnailRouter)
