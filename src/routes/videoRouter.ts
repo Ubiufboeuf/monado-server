c5269f756d24dfdfbbc1fa78f895e510172c7c4e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-10 13:54:01 -0300

    feat(video): :sparkles: implementar búsqueda de video por id

--- src/routes/videoRouter.ts
+++ src/routes/videoRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import { videoThumbnailRouter } from './videoThumbnailRouter'
-import { ROUTER_ERRORS, REQUEST_EXAMPLES } from '../lib/constants'
+import { ROUTER_ERRORS, REQUEST_EXAMPLES, VIDEO_ERRORS } from '../lib/constants'
 import { getMessage } from '../lib/displayMessages'
+import { getVideoById } from '../services/videoService'
 
 export const videoRouter = Router()
 
@@ -11,6 +12,36 @@ videoRouter.get('/', (_, res) => {
     success: false,
     message: getMessage(ROUTER_ERRORS.MISSING_VIDEO_ID),
     example: REQUEST_EXAMPLES.VIDEO
+  })
+})
+
+videoRouter.get('/:id', (req, res) => {
+  const { params } = req
+  const { id } = params
+  
+  if (!id) {
+    res.status(500)
+    res.json({
+      success: false,
+      message: getMessage(ROUTER_ERRORS.LOGICALLY_INNACCESIBLE_ROUTE)
+    })
+    return
+  }
+
+  const video = getVideoById(id)
+
+  if (!video) {
+    res.status(404)
+    res.json({
+      success: false,
+      message: getMessage(VIDEO_ERRORS.NOT_FOUND, id)
+    })
+
+    return
+  }
+
+  res.json({
+    ...params
   })
 })
 
---
+++ src/services/videoService.ts
@@ -0,0 +1,3 @@
+export function getVideoById (id: string) {
+  return null
+}
