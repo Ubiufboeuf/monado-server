4f343429c7278194b84419aab8ed786efb2fb0bc
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-10 13:45:33 -0300

    feat: :sparkles: convertir códigos de errores a mensajes humanamente legibles

---
+++ src/lib/displayMessages.ts
@@ -0,0 +1,17 @@
+import type { UserFacingMessageKey, UserFacingMessages } from '../messageTypes'
+
+const USER_FACING_MESSAGES: UserFacingMessages = {
+  ROUTER_ERROR_MISSING_VIDEO_ID: 'Falta especificar el ID del video',
+  ROUTER_ERROR_LOGICALLY_INNACCESIBLE_ROUTE: '¿Cómo llegaste hasta acá?',
+  VIDEO_ERROR_NOT_FOUND: 'No se encontró el video con ID:'
+}
+
+export function getMessage (errorCode: string, ...extra: string[]) {
+  const userMessage = USER_FACING_MESSAGES[errorCode as UserFacingMessageKey]
+
+  if (!extra.length) {
+    return userMessage ?? errorCode
+  }
+
+  return `${userMessage ?? errorCode} ${extra.join(' ')}`
+}
---
+++ src/messageTypes.ts
@@ -0,0 +1,7 @@
+import type { ROUTER_ERRORS, VIDEO_ERRORS } from './lib/constants'
+
+export type UserFacingMessageKey =
+  `ROUTER_ERROR_${keyof typeof ROUTER_ERRORS}`
+| `VIDEO_ERROR_${keyof typeof VIDEO_ERRORS}`
+
+export type UserFacingMessages = Record<UserFacingMessageKey, string>
