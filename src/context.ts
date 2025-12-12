8c938eb744b59c48b0b11ddb27910da5dc61d38a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-12 16:37:57 -0300

    feat(core): mejorar el contexto del servidor
    agregar la carpeta de assets e info para guardar sus contenidos

--- src/context.ts
+++ src/context.ts
@@ -2,9 +2,13 @@
 
 interface ServerContext {
   streamsFolder: Set<string>
+  assetsFolder: Set<string>
+  infoFolder: Set<string>
 }
 
 // export const serverContext = new AsyncLocalStorage<ServerContext>({ defaultValue: DefaultServerContext })
 export const serverContext: ServerContext = {
-  streamsFolder: new Set()
+  streamsFolder: new Set(),
+  assetsFolder: new Set(),
+  infoFolder: new Set()
 }
