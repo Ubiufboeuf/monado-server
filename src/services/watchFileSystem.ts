3da48b384449c40de3606b75c5a9a13bcf1769a1
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-12 16:45:27 -0300

    feat(file-system): :sparkles: agregar debounce para cargar los videos cuando cambia public/
    es más corto que el sleep() anterior porque no hace falta esperar tanto, y con el debounce se evitan actualizaciones del Map videos cuando hay varios cambios a la vez

--- src/services/watchFileSystem.ts
+++ src/services/watchFileSystem.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { watch } from 'node:fs/promises'
-import { updatePublicContent } from './publicContent'
+import { reloadPublicContent } from './publicContent'
 import { FS_ROUTES } from '../lib/constants'
 import { sleep } from 'bun'
+import { loadVideos } from './videoService'
 
 export async function watchPublicFolder () {
   const iterator = watch(FS_ROUTES.PUBLIC.fullPath, {
@@ -12,8 +14,24 @@ export async function watchPublicFolder () {
   for await (const { eventType, filename } of iterator) {
     if (eventType === 'rename') {
       // Esperar 1000s para evitar problemas de la caché del fs
-      await sleep(1000)
-      updatePublicContent(filename)
+      // await sleep(1000)
+      // loadVideos()
+      debouncedLoadVideos()
+      // reloadPublicContent(filename)
     }
   }
+}
+
+let timeout: NodeJS.Timeout | null = null
+const DEBOUNCE_DELAY = 400
+
+function debouncedLoadVideos () {
+  if (timeout) {
+    clearTimeout(timeout)
+  }
+
+  timeout = setTimeout(() => {
+    loadVideos()
+    timeout = null
+  }, DEBOUNCE_DELAY)
 }
