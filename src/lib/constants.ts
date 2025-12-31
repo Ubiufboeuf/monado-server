78d78bb902d6a5ed3bf39e2fc83feaff24b069bb
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-31 05:23:04 -0300

    feat(core): agregar constantes varias
    De errores, rutas (faltó para poster en un commit anterior), ejemplos y mensajes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,9 +7,13 @@ export const ERRORS = {
   MISSING_VIDEO_ID: 'MISSING_VIDEO_ID',
   LOGICALLY_INNACCESIBLE_ROUTE: 'LOGICALLY_INNACCESIBLE_ROUTE',
   VIDEO_NOT_FOUND: 'VIDEO_NOT_FOUND',
+  POSTER_NOT_FOUND: 'POSTER_NOT_FOUND',
   THUMBNAILS_NOT_FOUND: 'THUMBNAILS_NOT_FOUND',
   INTERNAL_FETCHING_THUMBNAILS: 'INTERNAL_FETCHING_THUMBNAILS',
-  THUMBNAIL_NOT_FOUND: 'THUMBNAIL_NOT_FOUND'
+  THUMBNAIL_NOT_FOUND: 'THUMBNAIL_NOT_FOUND',
+  BAD_STREAMS_REQUEST: 'BAD_STREAMS_REQUEST',
+  STREAM_NOT_FOUND: 'STREAM_NOT_FOUND',
+  INTERNAL_BAD_STREAM: 'INTERNAL_BAD_STREAM'
 } as const
 
 export const FS_ROUTES = createFsRoutes({
@@ -26,6 +30,7 @@ export const ROUTES = {
   VIDEO: {
     BASE: '/video',
     ID: '/:id',
+    POSTER: '/:id/poster',
     THUMBNAIL: '/:id/thumbnail',
     INFO: '/:id/info'
   }
@@ -33,14 +38,19 @@ export const ROUTES = {
 
 export const REQUEST_EXAMPLES = {
   VIDEO: `${ROUTES.VIDEO.BASE}/[ID]`,
-  THUMBNAIL: `${ROUTES.VIDEO.BASE}${ROUTES.VIDEO.THUMBNAIL}/[ID]`
+  THUMBNAIL: `${ROUTES.VIDEO.BASE}${ROUTES.VIDEO.THUMBNAIL}/[ID]`,
+  VIDEO_STREAM: `${ROUTES.STREAMS}/[ID]`
 } as const
 
 export const USER_FACING_MESSAGES: UserFacingMessages = {
   MISSING_VIDEO_ID: 'Falta especificar el ID del video',
   LOGICALLY_INNACCESIBLE_ROUTE: '¿Cómo llegaste hasta acá?',
   VIDEO_NOT_FOUND: 'No se encontró el video con ID:',
+  POSTER_NOT_FOUND: 'No se encontró el poster del video',
   THUMBNAILS_NOT_FOUND: 'No se encontraron las carátulas del video',
   INTERNAL_FETCHING_THUMBNAILS: 'Error interno al conseguir las carátulas del video',
-  THUMBNAIL_NOT_FOUND: 'No se encontró la carátula'
+  THUMBNAIL_NOT_FOUND: 'No se encontró la carátula',
+  BAD_STREAMS_REQUEST: 'Falta especificar el ID del video',
+  STREAM_NOT_FOUND: 'No se encontró el video',
+  INTERNAL_BAD_STREAM: 'Error interno al conseguir el stream del video'
 }
